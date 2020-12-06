251936b7e357df7dc9316a0ee60a3248a938ec83
AAGrigoriev <grigormaklaud@gmail.com>
2020-12-06 16:18:34 +0300

    add main

--- project_mvc/main.cpp
+++ project_mvc/main.cpp
@@ -5,13 +5,13 @@
 
 int main()
 {
-    //auto model_vec = std::make_shared<Model>();
-    //auto cont_vec  = std::make_shared<Controller>(model_vec);
-    //auto view_vec  = std::make_shared<View>(cont_vec,model_vec);
+    auto model_vec = std::make_shared<Model>();
+    auto cont_vec  = std::make_shared<Controller>(model_vec);
+    auto view_vec  = std::make_shared<View>(cont_vec,model_vec);
 
-    //model_vec->subscribe(view_vec);
-    //view_vec->button_add_dot();
-    //model_vec->unsubscribe(view_vec);
+    model_vec->subscribe(view_vec);
+    view_vec->button_add_dot();
+    model_vec->unsubscribe(view_vec);
     
     return 0;
 }
