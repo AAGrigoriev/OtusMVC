701398dc07d84ef775f91e1597b3553ca875dfd0
Andrei <grigormaklaud@gmai.com>
2021-01-20 09:09:51 +0300

    Revert "remove all"
    This reverts commit 9cc41a88b2dd3be6f3e0b01ac15d14fba2787ab6.

---
+++ project_mvc/core/model.cpp
@@ -0,0 +1,36 @@
+#include "model.hpp"
+#include "algorithm"
+
+void Model::save_document()
+{
+    /* save to file (Serialize)*/
+}
+
+void Model::open(std::string &FilePath)
+{
+    list_shapes.clear();
+
+    filePath = filePath;
+    /* load from file */
+    notify();
+}
+
+void Model::addPrimitive(std::unique_ptr<IShape> shape)
+{
+    list_shapes.push_back(std::move(shape));
+    notify();
+}
+
+void Model::deletePrimitive(std::unique_ptr<IShape> shape)
+{
+    list_shapes.remove(shape);
+    notify();
+}
+
+void Model::draw(std::shared_ptr<IGraphic> graphic)
+{
+    for(auto& object : list_shapes)
+    {
+        object->paint(graphic);
+    }
+}
